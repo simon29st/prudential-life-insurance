d9c3edef17737a60fe4d2d1b63682af28946b6e6
Simon <simon.stuerzebecher@campus.lmu.de>
2024-07-25 01:03:19 +0200

    restructured code

--- code.R
+++ code.R
@@ -59,6 +59,10 @@ one_hot_encode = function(data, col) {
   # one-hot-encode Product_Info_2 column (encoding via 'colapply' + 'encode' pipeops much slower)
   data %>% mutate(value = 1) %>% spread(col, value, fill = 0)  # cf. https://stackoverflow.com/a/52540145
 }
+
+
+################################################################################
+# pre-processing (1): discard NA cols
 data = removeNAcols(data)
 data = one_hot_encode(data, 'Product_Info_2')
 task = as_task_classif(data, target = 'Response')
@@ -67,15 +71,9 @@ split = partition(task, ratio = 0.8)
 task_train = as_task_classif(data, target = 'Response', row_ids = split$train)
 task_test = as_task_classif(data, target = 'Response', row_ids = split$test)
 
-
-# train a baseline random forest with 100 trees and max depth 5
-lrn_baseline = lrn('classif.ranger', num.trees = 100, max.depth = 5, id = 'rf')
-rr_baseline = resample(task_train, lrn_baseline, cv7)
-rr_baseline$aggregate(msr('classif.wkappa'))
-
-
 # approach (1): train simple multi-class classifier on entire task
-learners_1 = list(
+learners_11 = list(
+  lrn('classif.ranger', num.trees = 100, max.depth = 5, id = 'rf'),  # baseline
   lrn('classif.glmnet', id = 'elnet', alpha = 0.5, s = 0.01),
   lrn('classif.kknn', id = 'kknn'),
   lrn('classif.multinom', id = 'multinom', MaxNWts = 2000),
@@ -83,32 +81,36 @@ learners_1 = list(
   lrn('classif.xgboost', id = 'xgboost')
 )
 
-bg_1 = benchmark_grid(
+bg_11 = benchmark_grid(
   task = task_train,
-  learners = learners_1,
+  learners = learners_11,
   resamplings = cv7
 )
-b_1 = benchmark(bg_1)
-b_1$aggregate(msr('classif.wkappa'))
+b_11 = benchmark(bg_11)
+b_11$aggregate(msr('classif.wkappa'))
 
 
 # approach (2): perform one-vs-rest classification
-learners_2 = lapply(
-  X = learners_1,
+learners_12 = lapply(
+  X = learners_11,
   FUN = function(learner) {
-    pipeline_ovr(learner)
+    l_id = learner$id
+    learner = as_learner(pipeline_ovr(learner))
+    learner$id = paste0('ovr_', l_id)
+    learner
   }
 )
-bg_2 = benchmark_grid(
+bg_12 = benchmark_grid(
   task = task_train,
-  learners = learners_2,
+  learners = learners_12,
   resamplings = cv7
 )
-b_2 = benchmark(bg_2)
-b_2$aggregate(msr('classif.wkappa'))
+b_12 = benchmark(bg_12)
+b_12$aggregate(msr('classif.wkappa'))
 
 
-# approach (3): histogram-impute previously removed features + re-train both sets of learners
+################################################################################
+# pre-processing (2): histogram-impute NA vals
 data = read.csv('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/train.csv')
 data = one_hot_encode(data, 'Product_Info_2')
 task = as_task_classif(data, target = 'Response')
@@ -117,23 +119,48 @@ split = partition(task, ratio = 0.8)
 task_train = as_task_classif(data, target = 'Response', row_ids = split$train)
 task_test = as_task_classif(data, target = 'Response', row_ids = split$test)
 
-learners_3 = c(list(lrn_baseline), learners_1, learners_2)
-learners_3 = lapply(
-  X = learners_3,
+# approach (1): train simple multi-class classifier on entire task
+learners_21 = lapply(
+  X = learners_11,
   FUN = function(learner) {
-    as_learner(
+    l_id = learner$id
+    learner = as_learner(
       po('imputehist') %>>%
       learner
     )
+    learner$id = paste0('imputehist_', l_id)
+    learner
   }
 )
-bg_3 = benchmark_grid(
+bg_21 = benchmark_grid(
   task = task_train,
-  learners = learners_3,
+  learners = learners_21,
   resamplings = cv7
 )
-b_3 = benchmark(bg_3)
-b_3$aggregate(msr('classif.wkappa'))
+b_21 = benchmark(bg_21)
+b_21$aggregate(msr('classif.wkappa'))
+
+
+# approach (2): perform one-vs-rest classification
+learners_22 = lapply(
+  X = learners_12,
+  FUN = function(learner) {
+    l_id = learner$id
+    learner = as_learner(
+      po('imputehist') %>>%
+      learner
+    )
+    learner$id = paste0('imputehist_', l_id)
+    learner
+  }
+)
+bg_22 = benchmark_grid(
+  task = task_train,
+  learners = learners_22,
+  resamplings = cv7
+)
+b_22 = benchmark(bg_22)
+b_22$aggregate(msr('classif.wkappa'))
 
 
 ################################################################################
@@ -141,14 +168,12 @@ b_3$aggregate(msr('classif.wkappa'))
 ################################################################################
 
 
-# evaluation
+# submission
 # train models on entire dataset and test on unseen data
 write_to_csv = function(dt, filename) {
   dt[, 'Id'] = data_test[, 'Id']
   dt = dt[, Response:=as.integer(response)]
-  dt[, 'truth'] = NULL
-  dt[, 'row_ids'] = NULL
-  dt[, 'response'] = NULL
+  dt = dt[, c('Id', 'Response')]
   write.csv(
     dt,
     paste0('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/submissions/', filename, '.csv'),
@@ -156,7 +181,7 @@ write_to_csv = function(dt, filename) {
   )
 }
 
-# approaches (1), (2)
+# pre-processing (1)
 data = read.csv('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/train.csv')
 data = removeNAcols(data)
 data = one_hot_encode(data, 'Product_Info_2')
@@ -165,56 +190,59 @@ data_test = read.csv('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/
 data_test = removeNAcols(data_test)
 data_test = one_hot_encode(data_test, 'Product_Info_2')
 
-lrn_baseline$train(task)
-res_baseline = as.data.table(lrn_baseline$predict_newdata(data_test))
-write_to_csv(res_baseline, 'baseline-1')
-
-res_1 = lapply(
-  X = learners_1,
+res_11 = lapply(
+  X = learners_11,
   FUN = function(learner) {
     print(learner$id)
     learner$train(task)
-    as.data.table(learner$predict_newdata(data_test))
-  }
-)
-apply(
-  matrix(c(res_1, learners_1), ncol = 2),
-  MARGIN = 1,
-  FUN = function(row) {
-    write_to_csv(row[[1]], paste0(row[[2]]$id, '-1'))
+    res = as.data.table(learner$predict_newdata(data_test))
+    write_to_csv(res, paste0('res-11-', learner$id))
+    res
   }
 )
 
-res_2 = lapply(
-  X = learners_2,
+# OVR pipeline needs a response column with >= 2 levels: create dummy response column
+data_test['Response'] = round(runif(n = nrow(data_test), min = 1, max = 8), digits = 0)
+res_12 = lapply(
+  X = learners_12,
   FUN = function(learner) {
     print(learner$id)
     learner$train(task)
-    as.data.table(learner$predict_newdata(data_test))
-  }
-)
-apply(
-  matrix(c(res_2, learners_2), ncol = 2),
-  MARGIN = 1,
-  FUN = function(row) {
-    write_to_csv(row[[1]], paste0(row[[2]]$id, '-2'))
+    res = as.data.table(learner$predict_newdata(data_test))
+    write_to_csv(res, paste0('res-12-', learner$id))
+    res
   }
 )
 
 
-# approach (3)
+################################################################################
+# pre-processing (2)
 data = read.csv('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/train.csv')
 data = one_hot_encode(data, 'Product_Info_2')
 task = as_task_classif(data, target = 'Response')
 data_test = read.csv('C:/workplace/uni/ss-24/applied-ml/kaggle competition/data/test.csv')
 data_test = one_hot_encode(data_test, 'Product_Info_2')
-res_3 = lapply(
-  X = learners_3,
+
+res_21 = lapply(
+  X = learners_21,
   FUN = function(learner) {
     print(learner$id)
     learner$train(task)
     res = as.data.table(learner$predict_newdata(data_test))
-    write_to_csv(res, paste0(learner$id, '-3'))
+    write_to_csv(res, paste0('res-21-', learner$id))
+    res
+  }
+)
+
+# OVR pipeline needs a response column with >= 2 levels: create dummy response column
+data_test['Response'] = round(runif(n = nrow(data_test), min = 1, max = 8), digits = 0)
+res_22 = lapply(
+  X = learners_22,
+  FUN = function(learner) {
+    print(learner$id)
+    learner$train(task)
+    res = as.data.table(learner$predict_newdata(data_test))
+    write_to_csv(res, paste0('res-22-', learner$id))
     res
   }
 )
